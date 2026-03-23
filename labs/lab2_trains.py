26c340f262b01b6a8227291f84b2190a475b4628
Sophia Gotch <sophiegotch@users.noreply.github.com>
2026-03-23 20:16:11 +0000

    Completed Lab 03.1 - requests CRUD operations

---
+++ labs/lab2_trains.py
@@ -0,0 +1,37 @@
+import requests
+import csv
+from xml.dom.minidom import parseString
+
+url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+page = requests.get(url)
+doc = parseString(page.content)
+
+retrieveTags = [
+    "TrainStatus",
+    "TrainLatitude",
+    "TrainLongitude",
+    "TrainCode",
+    "TrainDate",
+    "PublicMessage",
+    "Direction"
+]
+
+with open("week03_train_full.csv", mode="w", newline="") as train_file:
+    train_writer = csv.writer(train_file, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    train_writer.writerow(retrieveTags)
+
+    objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+
+    for objTrainPositionsNode in objTrainPositionsNodes:
+        dataList = []
+
+        for retrieveTag in retrieveTags:
+            datanode = objTrainPositionsNode.getElementsByTagName(retrieveTag).item(0)
+
+            if datanode is not None and datanode.firstChild is not None:
+                dataList.append(datanode.firstChild.nodeValue.strip())
+            else:
+                dataList.append("")
+
+        train_writer.writerow(dataList)
